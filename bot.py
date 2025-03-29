a7a1e6d2f2b2c5358fb30ed985527b1a7d2ed181
sidkir <kirill.a.sidorov@gmail.com>
2025-03-29 20:40:19 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,18 @@ except Exception as e:
 @bot.message_handler(commands=['card'])
 def send_card(message):
     card = random.choice(cards)
-    if "file_id" in card:
+
+    if "file_ids" in card:
+        # Отправляем все картинки по очереди
+        for file_id in card["file_ids"]:
+            bot.send_photo(message.chat.id, file_id)
+        # Потом отправляем текст
+        bot.send_message(message.chat.id, f"{card['name']}\n\n{card['description']}")
+
+    elif "file_id" in card:
+        # Если только одна картинка
         bot.send_photo(message.chat.id, card["file_id"], caption=f"{card['name']}\n\n{card['description']}")
+
     else:
+        # Если нет картинок
         bot.send_message(message.chat.id, f"{card['name']}\n\n{card['description']}")
