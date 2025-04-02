4f04170f2ded61181d4e7cc7bde43518af5c3d12
sidkir <kirill.a.sidorov@gmail.com>
2025-04-02 21:01:05 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -214,8 +214,8 @@ def send_random_text(message, filename, label):
         bot.send_message(message.chat.id, f"Список пуст — {label}")
         return
     text = random.choice(cards).get("text", "")
-    bot.send_message(message.chat.id, f"{label}:
-{text}")
+    bot.send_message(message.chat.id, f"{label}:{text}")
+
 
 @app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
