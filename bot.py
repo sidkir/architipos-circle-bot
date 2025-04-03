10f6a479882746ec2de10b960637b850173994d6
sidkir <kirill.a.sidorov@gmail.com>
2025-04-03 10:14:08 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -228,3 +228,4 @@ if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=f"https://{os.environ['RENDER_EXTERNAL_HOSTNAME']}/{TOKEN}")
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
